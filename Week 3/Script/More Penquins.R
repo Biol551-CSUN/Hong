1dcfdf4eb424d76dffbc9706b7f70b8617c694b2
HonglabcsunGit <ray.hong@csun.edu>
2022-02-10 18:09:57 -0800

    update

--- Week 3/Script/More Penquins.R
+++ Week 3/Script/More Penquins.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(palmerpenguins)
 library(here)
 library(beyonce)
+library(calecopal)
 library(ggthemes)
 glimpse(penguins)
 ggplot(data=penguins,
@@ -15,21 +16,22 @@ ggplot(data=penguins,
                      color=species))+
                      geom_point()+
                      geom_smooth(method = 'lm')+
-                       labs(x="Bill depth (mm)",
-                            y= "Bill length (mm)"
-                            )+
+                      labs(x="Bill depth (mm)",
+                        y="Bill length (mm)"
+                        )+
 #scale_color_viridis_d()+ ###change color for color blind
 #scale_color_manual(values = c("orange", "purple", green")+ ###only one scale at a time
-#scale_color_manual(values=beyonce_palette(27))+ #up to 130 palettes
-theme_economist()+
-  scale_colour_economist()
+#scale_color_manual(values=beyonce_palette(22)) #up to 130 palettes
+scale_color_manual(values = cal_palette("chaparral1"))+
+theme_bw() +
+#theme_economist()+
+#scale_colour_economist()
   #theme_bw()
 #theme_classic()
-#theme(axis.title=element_text(size=25, 
- #                             color="red"),
-  #    panel.background=element_rect(fill='linen'))+
-  coord_fixed()
-ggsave(here("Week 3", "output", "penguins_economist.png"))
+theme(axis.title=element_text(size=20, 
+   color="Blue"),
+   panel.background=element_rect(fill='linen'))
+#ggsave(here("Week 3", "output", "penguins_stata.png"))
 #theme_bw() #gray background
 #coord_flip() #flip x and y axes
 #coord_polar("x") # make polar                   
